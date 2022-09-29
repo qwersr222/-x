cf2da7e365028739e0b73cf223bd4cdb0b0d1dec
qwersr222 <88492913+qwersr222@users.noreply.github.com>
2022-09-29 21:53:03 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -10,5 +10,5 @@ https:\/\/cat\-match\.easygame2021\.com\/sheep\/v1\/game\/personal\_info\? scrip
 hostname = cat-match.easygame2021.com
 
 */
-body = $response.body.replace(/win_count":\d+/g , 'win_count":99999999999999999').replace(/topic_count":\d+/g , 'topic_count":1').replace(/today_state":\d+/g , 'today_state":999999999').replace(/today_time":\d+/g , 'today_time":0.1');
+body = $response.body.replace(/win_count":\d+/g , 'win_count":5201314').replace(/topic_count":\d+/g , 'topic_count":1').replace(/today_state":\d+/g , 'today_state":5201314').replace(/today_time":\d+/g , 'today_time":5201314');
 $done({body});
