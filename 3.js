e3f08a29a5f9fea786f8d678797d8f179299cda5
qwersr222 <88492913+qwersr222@users.noreply.github.com>
2022-09-29 20:09:59 +0800

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,14 @@
+/*
+[rewrite_local]
+#本地重写
+
+#重写路径
+https:\/\/cat\-match\.easygame2021\.com\/sheep\/v1\/game\/personal\_info\? script-response-body 3.js
+
+[MITM]
+#域名
+hostname = cat-match.easygame2021.com
+
+*/
+body = $response.body.replace(/win_count":\d+/g , 'win_count":99999999999999999').replace(/topic_count":\d+/g , 'topic_count":1').replace(/today_state":\d+/g , 'today_state":999999999').replace(/today_time":\d+/g , 'today_time":0.1');
+$done({body});
